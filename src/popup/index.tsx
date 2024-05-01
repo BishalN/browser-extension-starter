a31d81efe73b70b400f70683b3d884fa2c2fbe6b
Bishal Neupane <Neupanebishal07@gmail.com>
2024-05-01 10:25:12 +0545

    Update parcel to latest and Add shadcn ui

--- src/popup/index.tsx
+++ src/popup/index.tsx
@@ -1,6 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Button } from "../components/ui/button";
 
 ReactDOM.createRoot(document.getElementById("app")!).render(
-  <div className="bg-gray-500 text-white">Popup page here</div>
+  <div className="bg-gray-500 text-white">
+    Popup page here
+    <Button onClick={() => alert("Hello")}>Click me</Button>
+  </div>
 );
